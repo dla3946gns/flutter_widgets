af1d6ebc39fdb9de78337ef8d5d5a5b4a69e52de
mhlim <dla3946gns@naver.com>
2024-07-16 00:11:40 +0900

    feat: carousel_slider 대신 flutter_carousel_widget을 사용하도록 마이그레이션 완료

--- lib/flutter_carousel.dart
+++ lib/flutter_carousel.dart
@@ -1,38 +1,9 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/flutter_carousel.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:flutter_widgets/main.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class FlutterCarouselState extends State<MyHomePage> {
   final List<String> imgList = [
     "https://cdn.open-pr.com/T/5/T507166583_g.jpg",
     "https://www.retcustomsbrokerage.com/wp-content/themes/retcustoms/images/resource/br.jpg",
@@ -63,19 +34,17 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Carousel Slider Example',
+          'Flutter Carousel Widget',
         ),
       ),
       body: Container(
         width: MediaQuery.of(context).size.width,
-        child: CarouselSlider(
+        child: FlutterCarousel(
           options: CarouselOptions(
             height: 430,
+            showIndicator: false,
+            slideIndicator: CircularSlideIndicator(),
             enableInfiniteScroll: true,
-            initialPage: 0,
-            onPageChanged: (index, reason) {
-              print('Page changed to $index');
-            },
             disableCenter: true,
             viewportFraction: getViewportFraction(),
             padEnds: false,
